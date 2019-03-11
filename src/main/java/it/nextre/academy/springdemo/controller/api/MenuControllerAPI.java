7a2aa4291992921569c186369def940dc39a5f80
valix85@gmail.com <valix85@gmail.com>
2019-03-11 12:01:58 +0100

    fix name controller API

--- src/main/java/it/nextre/academy/springdemo/controller/api/MenuControllerAPI.java
+++ src/main/java/it/nextre/academy/springdemo/controller/api/MenuControllerAPI.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class MenuController {
+public class MenuControllerAPI {
 
     @Autowired
     TopDealService topDealService;
